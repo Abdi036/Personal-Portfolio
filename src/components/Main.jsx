5826ff9e40b48f4525fb357c9fb9a8ed0b181d30
Abdi Kumela <abdikumela036@gmail.com>
2024-04-06 13:19:53 +0300

    styling the service section

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -18,6 +18,57 @@ const skills = [
     experience: 70,
   },
 ];
+
+// SERVICES
+const services = [
+  {
+    service: "UI/UX Web Designing",
+    icon: "fa-solid fa-pen-ruler",
+    description:
+      "As a UI/UX Designer, my focus is on understanding users' needs and crafting intuitive experiences that enhance usability and satisfaction. I combine my knowledge of design principles, user research, and industry best practices to create wireframes, prototypes, and visual designs that effectively communicate the brand's message and guide users through seamless interactions. By considering aspects such as information architecture, visual hierarchy, and user flows, I aim to optimize the overall user experience and create interfaces that are both aesthetically pleasing and highly functional.",
+  },
+  {
+    service: "Web developement",
+    icon: "fa-solid fa-desktop",
+    description:
+      " I am proficient in front-end development, implementing user interfaces and responsive designs that ensure optimal user experiences across various devices. Additionally, I have expertise in back-end development, enabling me to handle server-side logic, database management, and API integrations.",
+  },
+  {
+    service: "App Designinig",
+    icon: "fa-solid fa-mobile",
+    description:
+      "As an App Designer, I combine my knowledge of user experience (UX) principles, user research, and industry trends to craft engaging and seamless app experiences. I pay attention to details such as information architecture, navigation patterns, and visual hierarchy to create intuitive user flows that guide users through the app effortlessly. By focusing on usability and aesthetics, I aim to create visually stunning designs that not only captivate users but also enhance their overall satisfaction and engagement with the app.",
+  },
+  {
+    service: "App Developement",
+    icon: "fa-brands fa-android",
+    description:
+      "I am proficient in implementing user interfaces, handling data management, integrating with backend systems, and ensuring optimal performance and security. With a focus on user experience (UX) and usability, I strive to create intuitive and engaging mobile apps that provide seamless functionality and an enjoyable experience for users.",
+  },
+];
+
+const myWorks = [
+  {
+    title: "Bankist",
+    description: "ncjndvlwnvvvvvvvdhcbakbb",
+    image: "pexels-pixabay-50987.jpg",
+  },
+  {
+    title: "Forkify-Food-Recipe",
+    description: "ncjndvlwnvvvvvvvdhcbakbb",
+    image: "pexels-ella-olsson-1640777.jpg",
+  },
+  {
+    title: "WorldWise",
+    description: "ncjndvlwnvvvvvvvdhcbakbb",
+    image: "pexels-kaichieh-chan-917510.jpg",
+  },
+  {
+    title: "UsePopCorn",
+    description: "ncjndvlwnvvvvvvvdhcbakbb",
+    image: "pexels-donald-tong-109669.jpg",
+  },
+];
 export default function Main() {
   return (
     <div className={styles.main}>
@@ -32,6 +83,8 @@ export default function Main() {
   );
 }
 
+// //////////////////////////////////////////////////////////////////
+
 function Welcome() {
   return (
     <section className={styles.welcome}>
@@ -45,6 +98,7 @@ function Welcome() {
   );
 }
 
+// ///////////////////////////////////////////////////////////////////////
 function AboutMe() {
   return (
     <section className={styles.aboutme}>
@@ -93,73 +147,43 @@ function ProgressBar() {
   );
 }
 
+// //////////////////////////////////////////////////////////////////////
+
 function Service() {
   return (
     <section className={styles.servicesSection}>
       <h1>Services</h1>
       <div className={styles.services}>
-        <div className={styles.service}>
-          <h2>Web Design</h2>
-          <p>
-            Lisque persius interesset his et, in quot quidam persequeris vim, ad
-            mea essent possim iriure.
-          </p>
-        </div>
-        <div className={styles.service}>
-          <h2>UI/UX Design</h2>
-          <p>
-            Lisque persius interesset his et, in quot quidam persequeris vim, ad
-            mea essent possim iriure.
-          </p>
-        </div>
-        <div className={styles.service}>
-          <h2>App Design & Develop</h2>
-          <p>
-            Lisque persius interesset his et, in quot quidam persequeris vim, ad
-            mea essent possim iriure.
-          </p>
-        </div>
+        {services.map((service) => (
+          <div
+            className={styles.service}
+            style={{ background: `${service.image}` }}
+          >
+            <h2>
+              <i className={`${service.icon} ${styles.icons}`}></i>
+              {service.service}
+            </h2>
+            <p>{service.description}</p>
+          </div>
+        ))}
       </div>
     </section>
   );
 }
+// //////////////////////////////////////////////////////////////
 
 function Portfolio() {
   return (
     <section className={styles.portfolio}>
       <h1>Portfolio</h1>
-      <h2>My Work</h2>
-      <div className={styles.filters}>
-        <button>All</button>
-        <button>Design</button>
-        <button>Brand</button>
-        <button>Photos</button>
-      </div>
+      <h2>My Works</h2>
       <div className={styles.projects}>
-        <div className={styles.project}>
-          <h3>Project Title</h3>
-          <p>Category</p>
-        </div>
-        <div className={styles.project}>
-          <h3>Project Title</h3>
-          <p>Category</p>
-        </div>
-        <div className={styles.project}>
-          <h3>Project Title</h3>
-          <p>Category</p>
-        </div>
-        <div className={styles.project}>
-          <h3>Project Title</h3>
-          <p>Category</p>
-        </div>
-        <div className={styles.project}>
-          <h3>Project Title</h3>
-          <p>Category</p>
-        </div>
-        <div className={styles.project}>
-          <h3>Project Title</h3>
-          <p>Category</p>
-        </div>
+        {myWorks.map((work) => (
+          <div className={styles.project}>
+            <h3>{work.title}</h3>
+            <p>{work.description}</p>
+          </div>
+        ))}
       </div>
     </section>
   );
@@ -190,7 +214,7 @@ function Testimonials() {
               </div>
             </div>
             <div className={styles.testimonialImage}>
-              <img src="./public/img-1.jpg" alt="ProfilePic" />
+              <img src="/img-1.jpg" alt="ProfilePic" />
             </div>
           </div>
         </div>
